93d67470276ec692e539f4e8414ae3974d8b1288
kimstars <buikika9@gmail.com>
2024-07-09 15:03:45 +0700

    Add project files.

---
+++ BreakReminder/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace BreakReminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timeremider = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.btnSettime = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("LEMON MILK Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BREAK REMINDER";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(342, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bạn đã sử dụng máy tính liên tục trong: ";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(50, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(293, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhắc nhở bạn nghỉ ngơi khi sử dụng máy tính";
+            // 
+            // timeremider
+            // 
+            this.timeremider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.timeremider.AutoSize = true;
+            this.timeremider.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeremider.Location = new System.Drawing.Point(90, 145);
+            this.timeremider.Name = "timeremider";
+            this.timeremider.Size = new System.Drawing.Size(229, 46);
+            this.timeremider.TabIndex = 3;
+            this.timeremider.Text = "00:00/30:00";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(325, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Hãy nghỉ ngơi để bảo vệ mắt của bạn";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(413, 344);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(405, 315);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "BreakReminder";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.timeremider);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 309);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.txtTime);
+            this.tabPage2.Controls.Add(this.btnSettime);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(405, 315);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Setting";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(208, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "phút";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(54, 84);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(148, 22);
+            this.txtTime.TabIndex = 2;
+            this.txtTime.Text = "30";
+            // 
+            // btnSettime
+            // 
+            this.btnSettime.Location = new System.Drawing.Point(274, 79);
+            this.btnSettime.Name = "btnSettime";
+            this.btnSettime.Size = new System.Drawing.Size(75, 33);
+            this.btnSettime.TabIndex = 1;
+            this.btnSettime.Text = "Thiết lập";
+            this.btnSettime.UseVisualStyleBackColor = true;
+            this.btnSettime.Click += new System.EventHandler(this.btnSettime_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(49, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(314, 29);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Cài đặt thời gian nhắc nhở";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(123, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 17);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "CHU TUAN KIET - 2024";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(413, 344);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Break Reminder";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label timeremider;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button btnSettime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ BreakReminder/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BreakReminder
+{
+    public partial class Form1 : Form
+    {
+        private Timer mainTimer;
+        private Timer displayTimer;
+        private NotifyIcon notifyIcon;
+        private ContextMenuStrip contextMenuStrip;
+        private ToolStripMenuItem exitMenuItem;
+        private DateTime startTime;
+        private int TimeSetup = 30;
+        private TimeSpan interval = TimeSpan.FromMinutes(30);
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimers();
+            InitializeNotifyIcon();
+        }
+
+        private void InitializeTimers()
+        {
+            
+            mainTimer = new Timer();
+            mainTimer.Interval = (int)interval.TotalMilliseconds;
+            mainTimer.Tick += MainTimer_Tick;
+            mainTimer.Start();
+
+            displayTimer = new Timer();
+            displayTimer.Interval = 1000; // 1 giây
+            displayTimer.Tick += DisplayTimer_Tick;
+            displayTimer.Start();
+
+            startTime = DateTime.Now;
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            mainTimer.Stop(); // Dừng timer
+            ShowReminder();
+        }
+
+        private void DisplayTimer_Tick(object sender, EventArgs e)
+        {
+            var elapsedTime = DateTime.Now - startTime;
+
+            timeremider.Text = $"{elapsedTime:mm\\:ss}/{interval:mm\\:ss}";
+        }
+
+        private void ShowReminder()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Minimized;
+            this.WindowState = FormWindowState.Normal;
+            this.BringToFront();
+            notifyIcon.ShowBalloonTip(500,"Nhắc nhở!!", $"Bạn đã sử dụng máy tính {TimeSetup} phút. Hãy nghỉ ngơi! Vận động cơ thể nhé", ToolTipIcon.Warning);
+            var result = MessageBox.Show($"Bạn đã sử dụng máy tính {TimeSetup} phút. Hãy nghỉ ngơi! Vận động cơ thể nhé", "Nhắc nhở", MessageBoxButtons.OK);
+            if (result == DialogResult.OK)
+            {
+                var confirmResult = MessageBox.Show("Bạn đã vận động xong chưa và có thể tiếp tục làm việc không?", "Xác nhận", MessageBoxButtons.OK);
+                if (confirmResult == DialogResult.OK)
+                {
+                    startTime = DateTime.Now; // Reset thời gian bắt đầu
+                    mainTimer.Start(); // Reset và bắt đầu lại timer
+                }
+            }
+        }
+
+        private void InitializeNotifyIcon()
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = this.Icon;
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+
+            contextMenuStrip = new ContextMenuStrip();
+            exitMenuItem = new ToolStripMenuItem("Thoát", null, ExitMenuItem_Click);
+            contextMenuStrip.Items.Add(exitMenuItem);
+
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+  
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            this.ShowInTaskbar = false;
+            base.OnFormClosing(e);
+        }
+
+        private void btnSettime_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtTime.Text, out TimeSetup))
+            {
+                MessageBox.Show("Vui lòng chỉ nhập số phút");
+                return;
+            }
+
+            interval = TimeSpan.FromMinutes(TimeSetup);
+            mainTimer.Interval = (int)interval.TotalMilliseconds;
+
+            startTime = DateTime.Now; // Reset thời gian bắt đầu
+            mainTimer.Start(); // Reset và bắt đầu lại timer
+        }
+    }
+}
